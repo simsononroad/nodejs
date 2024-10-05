a947f166ff0181887dc54f6da2c1eb1773934bd4
simsononroad <d-github@kopaszhegy.hu>
2024-10-05 14:38:23 +0200

    roviditesek

--- feedreader.mjs
+++ feedreader.mjs
@@ -3,25 +3,25 @@ import { close, question } from './rl.mjs'
 
 const feeds = await getLinks()
 
-let input =  await question('Írj be egy parancsot (list, add, del, read, save/quit): ')
+let input =  await question('Írj be egy parancsot (list, add, del, read, save/quit(s/q)): ')
 
-while (input != 'save/quit'){
+while (input != 's/q'){
     let cmdParts = input.trim().split(' ')
     let cmd = cmdParts[0]
     if (cmd == 'list'){
         feeds.forEach((url, index) => console.log(`${index}\t${url}`))
     }
     if(cmd == 'add'){
-        if(cmdParts.length < 2){
-            console.log('Kérlek add meg az "url"-t az "add" parancs segítségével')
+        if(cmdParts.length < 2 || cmdParts == ".help"){
+            console.log('Kérlek add meg az "url"-t az "add" parancs segítségével és a "https://" nélkül')
         }else{
-            feeds.push(cmdParts[1])
+            feeds.push("https://" + cmdParts[1])
         }
     }
 
 
     
-    input = await question('Írj be egy parancsot (list, add, del, read, save/quit): ')
+    input = await question('Írj be egy parancsot (list, add, del, read, save/quit(s/q)): ')
 }
 
 await saveLinks(feeds)
